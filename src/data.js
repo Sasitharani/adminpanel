d8b977f6fefcd3cbbda3a06706f3e52da04ccda5
sasi <sasitharani@gmail.com>
2024-08-27 14:41:15 +0530

    merged

---
+++ src/data.js
@@ -0,0 +1,25 @@
+// src/data.js
+import { faHome, faUser, faCog } from '@fortawesome/free-solid-svg-icons';
+
+const data = [
+  {
+    name: 'Color',
+    icon: faHome,
+    ref1: '#home1',
+    ref2: '#home2'
+  },
+  {
+    name: 'Size',
+    icon: faUser,
+    ref1: '#profile1',
+    ref2: '#profile2'
+  },
+  {
+    name: 'Parent',
+    icon: faCog,
+    ref1: '#settings1',
+    ref2: '#settings2'
+  }
+];
+
+export default data;
